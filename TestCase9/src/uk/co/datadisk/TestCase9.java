75f2c1ebec421ffd782fc37052f3573df2c12705
pvalle <paul.valle@orbian.com>
2018-12-03 13:33:10 +0000

    playing around with actions, drag and drop

---
+++ TestCase9/src/uk/co/datadisk/TestCase9.java
@@ -0,0 +1,33 @@
+package uk.co.datadisk;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class TestCase9 {
+
+    // Testing Frames and Drag/Drop
+
+    static final String BASE_URL = "http://jqueryui.com/droppable";
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+        WebDriver cDriver =new ChromeDriver();
+        cDriver.get(BASE_URL);
+
+        // Identify the Frame
+        cDriver.switchTo().frame(cDriver.findElement(By.className("demo-frame")));
+        cDriver.findElement(By.id("draggable")).click();
+
+        // Now drag the source box to the destination box
+        Actions actions = new Actions(cDriver);
+
+        WebElement source_box = cDriver.findElement(By.id("draggable"));
+        WebElement destination_box = cDriver.findElement(By.id("droppable"));
+
+        actions.dragAndDrop(source_box, destination_box).build().perform();
+    }
+}
