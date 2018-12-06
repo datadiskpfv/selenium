0fca47467002b427ef15a2114513c14a5cc26a4d
pvalle <paul.valle@orbian.com>
2018-12-06 08:50:26 +0000

    creating screenshots

--- TestCase15/src/uk/co/datadisk/TestCase15.java
+++ TestCase15/src/uk/co/datadisk/TestCase15.java
@@ -1,15 +1,20 @@
 package uk.co.datadisk;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+
 public class TestCase15 {
 
-    // Maximize the browser window and delete cookies
+    // Maximize the browser window, delete cookies, screenshots
 
     static final String BASE_URL = "http://www.datadisk.co.uk";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
 
@@ -21,8 +26,12 @@ public class TestCase15 {
 
         // delete all the cookies (use deleteCookieNamed for specific cookies)
         cDriver.manage().deleteAllCookies();
+        //cDriver.manage().deleteCookieNamed("<cookie name>");
 
         cDriver.get(BASE_URL);
 
+        // capture a screenshot
+        File src = ((TakesScreenshot) cDriver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(src, new File("C:\\Users\\pvalle\\Pictures\\screenshot.png"));
     }
 }
